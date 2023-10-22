eada188e2b11470277688809e22e009c20eee7cf
bpetra10 <bodnar.petra9@gmail.com>
2023-10-22 16:46:51 +0200

    WallOfGlory images, testing some randomization without login and point check

--- Dogs/Dogs/WallOfGlory/WallOfGlory.xaml.cs
+++ Dogs/Dogs/WallOfGlory/WallOfGlory.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,11 @@ namespace Dogs.WallOfGlory
     /// </summary>
     public partial class WallOfGlory : Page
     {
+        private List<Image> images;
         public WallOfGlory()
         {
             InitializeComponent();
+            images = new List<Image>() { agar, kopo, bulldog, komondor, kuvasz, mudi, njuhasz, puli, pumi, roti, vizsla };
         }
 
         private void BackToLearn_Click(object sender, RoutedEventArgs e)
@@ -33,7 +36,16 @@ namespace Dogs.WallOfGlory
 
         private void Raffle_Click(object sender, RoutedEventArgs e)
         {
-
+            Random rnd = new Random();
+            if (images.Count > 0)
+            {
+                var randNum = rnd.Next(0, images.Count);
+                images[randNum].Visibility = Visibility.Visible;
+                images.Remove(images[randNum]);
+            }
+            else {
+                MessageBox.Show("Kigyűjtötted a képeket!");
+            }
         }
     }
 }
