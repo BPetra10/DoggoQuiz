d3e4037360f3d8f664200bfaaee46f570cbb54e2
bpetra10 <bodnar.petra9@gmail.com>
2023-11-05 17:05:07 +0100

    Adding Quiz.xaml, Shuffle algorithm, nextQuestion method etc.

--- Dogs/Dogs/Game/Quiz.xaml.cs
+++ Dogs/Dogs/Game/Quiz.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dogs.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,20 +21,76 @@ namespace Dogs.Game
     /// </summary>
     public partial class Quiz : Page
     {
-        string selectedDogs;
+        List<Question> collection;
         public Quiz(string selectedDogs)
         {
             InitializeComponent();
-            this.selectedDogs = selectedDogs;
+
+            //Getting selected dogs questions and answers from DB, and Shuffling its sequence.
+            DB.DB database = new DB.DB();
+            collection = database.GetQuestions(selectedDogs);
+            Shuffle(collection);
         }
 
+        static Random rnd = new Random();
+        static void Shuffle<T>(List<T> collection)
+        {
+            /*Fisher-Yates Shuffle Algorithm for randomizing.*/
+            for (int i = collection.Count - 1; i > 0; --i)
+            {
+                int k = rnd.Next(i + 1);
+                var temp = collection[i];
+                collection[i] = collection[k];
+                collection[k] = temp;
+            }
+        }
+         void NextQuestionWithAns(int index) 
+         {
+            question.Text = collection[index].question;
+
+            List<string> answers = new List<string>() { collection[index].answer1, collection[index].answer2, 
+                collection[index].answer3, collection[index].correct };
+
+            Shuffle(answers);
+
+            ans1.Text = answers[0];
+            ans2.Text = answers[1];
+            ans3.Text = answers[2];
+            ans4.Text = answers[3];
+        }
         private void QuizGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            DB.DB database = new DB.DB();
-            var collection = database.GetQuestions(selectedDogs);
-            foreach (var item in collection)
+            NextQuestionWithAns(0);
+        }
+
+        //TODO:Checking wich button has the correct answer maybe get ButtonList and check like that?
+        /*
+        private void CheckIfCorrect(object sender, Button senderBtn) {
+            if (sender.Equals(senderBtn))
             {
-                test.Content = test.Content + "\n" + item.question;
+                if (senderBtn.Content.ToString() == collection[i].correct)
+                {
+                    senderBtn.Background = new SolidColorBrush(Colors.Green);
+                }
+                else
+                {
+                    senderBtn.Background = new SolidColorBrush(Colors.Red);
+
+                }
+            }
+        }
+        */
+
+        int i = 0;
+        private void Btn(object sender, RoutedEventArgs e)
+        {
+            i++;
+            if (i != collection.Count)
+            {
+                NextQuestionWithAns(i);
+            }
+            else {
+                MessageBox.Show("Vége");
             }
         }
     }
